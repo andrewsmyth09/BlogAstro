6fa6397dc5d9dd7dddab135975496a807c2d4065
andrewsmyth09 <asmyth@gmx.com>
2025-09-17 13:54:47 +0200

    Final stage complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://chic-phoenix-8e039a.netlify.app/"
-});
+  site: "https://chic-phoenix-8e039a.netlify.app/",
+  integrations: [preact()]
+}); 
